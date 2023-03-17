b9eb5a0e302eb5f5169ad6933478a732202bc1b6
AffraAdiWirawan <affrawirawan9@gmail.com>
2023-03-17 12:31:34 +0700

    First Commit

---
+++ Act1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double Luas, p, l;
+    cout << " Masukkan Panjangnya = ";
+    cin >> p;
+    cout << " Masukkan Lebarnya = ";
+    cin >>l;
+    Luas = p*l;
+    cout << " Luasnya = " << Luas;
+}
